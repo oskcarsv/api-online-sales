585cd715706ff418dc0565006223e9ca949d0efb
oskcarsv <oskcarqsv@gmail.com>
2024-03-08 10:10:00 -0600

    create category.model.js

---
+++ src/category/category.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const CategorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "The name is required"],
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+export default mongoose.model('Category', CategorySchema);
+
